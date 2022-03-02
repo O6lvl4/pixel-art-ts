bf4496ce2f9284c2cb1be539d549210d53c48695
O6lvl4 <motodera@phismize.com>
2022-03-03 03:29:32 +0900

    Add converting json to ts-class definition

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { Pixel } from './pixel'
+
 export const hello = (): void => {
-  console.log("Hello, World!");
+  const p: Pixel = {
+    colorSet: new Map().set(1, "#fff").set(2, "#000"),
+    size: {
+      width: 2,
+      height: 2
+    },
+    data: [
+      [0, 1],
+      [2, 0]
+    ]
+  }
+  console.log(p);
 };
